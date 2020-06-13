5a43ea747a9e53c2abaf19b0ad5578fc4b172c17
neha-sankad <60845183+neha-sankad@users.noreply.github.com>
2020-06-13 14:16:36 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,12 @@
+filename=input("enter the filename:")
+ext=filename.split(".")
+if ext[-1]=="py":
+    print("the extension of file is: python")
+elif(ext[-1]=="c"):
+    print("the extension of file is: c")
+elif(ext[-1]=="java"):
+    print("the extension of file is: java")
+elif(ext[-1]=="cpp"):
+    print("the extension of file is: c++")
+
+    
